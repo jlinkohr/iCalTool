56e3fb45ae5acce65f543019010b000ec0f992e1
jlinkohr <44720939+jlinkohr@users.noreply.github.com>
2022-12-29 18:34:52 +0100

    Create iCalConvert.py

---
+++ iCalConvert.py
@@ -0,0 +1,26 @@
+# adjust iCal files e.g. notifications
+# 2022 Jochen Linkohr
+# start code done with help of Chat GPT
+
+import icalendar
+from datetime import timedelta
+
+# Open the input .ics file
+with open('calendar.ics', 'rb') as f:
+    # Parse the file into a Calendar object
+    calendar = icalendar.Calendar.from_ical(f.read())
+
+# Loop through all events in the calendar
+for component in calendar.walk():
+    # Check if the component is an event
+    if component.name == "VEVENT":
+        # add alarms
+        alarm = icalendar.Alarm()
+        trigger = -timedelta(hours=5)
+        alarm.add('TRIGGER', trigger)
+        component.add_component(alarm)
+      
+#Open a new .ics file for writing
+with open('modified_calendar.ics', 'wb') as f:
+    # Write the modified calendar to the file
+    f.write(calendar.to_ical())
